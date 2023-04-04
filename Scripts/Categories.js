3b97dcae1072cabe6d0dbfaf1066936d87001eb6
Lioteron <lioteronex@gmail.com>
2023-04-04 11:40:04 +0200

    Added functionality to categories

---
+++ Scripts/Categories.js
@@ -0,0 +1,31 @@
+const subCategories = document.querySelectorAll("optgroup");
+const mainSelectDropdown = document.querySelector("#main-select");
+const subSelectDropdown = document.querySelector("#sub-select");
+console.log(subSelectDropdown);
+
+function showCategories(category) {
+    let categoryID = category.id;
+
+    subCategories.forEach((ele) => {
+        if (!ele) return;
+
+        if (ele.id == categoryID && ele.hasAttribute("hidden")) {
+            console.log(ele);
+            ele.removeAttribute("hidden");
+        } else if (ele.id == categoryID && !ele.hasAttribute("hidden")) {
+            ele.setAttribute("hidden", "hidden");
+        }
+    });
+}
+
+function hideCategories() {
+    subCategories.forEach((e) => e.setAttribute("hidden", "hidden"));
+    subSelectDropdown.value = "";
+}
+
+mainSelectDropdown.addEventListener("change", function () {
+    let selectedCategory = mainSelectDropdown.options[mainSelectDropdown.selectedIndex];
+
+    hideCategories();
+    showCategories(selectedCategory);
+});
